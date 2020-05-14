cde5c1c14f96657b6ae60439ac7df58ff877ca27
BernardoG <A00570682@itesm.mx>
2020-05-14 15:01:12 -0500

    Dia 1. Inicio de proyecto con clases Fecha y Reserva

---
+++ Proyecto 3/Reserva.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "Fecha.h"
+
+using namespace std;
+
+class Reserva
+{
+public:
+	Reserva();
+	Reserva(int idm, int idc, Fecha f);
+
+	void setIdM(int idm) { idMaterial = idm; }
+	void setIdC(int idc) { idCliente = idc; }
+	void setFechaReserv(Fecha f) { fechaReservacion = f; }
+
+	int getIdM() { return idMaterial; }
+	int getIdC() { return idCliente; }
+	Fecha getFechaReserv() { return fechaReservacion; }
+
+	//calculaFechabla aun no esta terminado.
+	void calculaFechaFinReserva();
+
+private:
+	int idMaterial, idCliente;
+	Fecha fechaReservacion;
+};
+
+Reserva::Reserva()
+{
+	idMaterial = 0;
+	idCliente = 0;
+	
+	Fecha f(0, 0, 0);
+	fechaReservacion = f;
+}
+
+Reserva::Reserva(int idm, int idc, Fecha f)
+{
+	idMaterial = idm;
+	idCliente = idc;
+	fechaReservacion = f;
+}
